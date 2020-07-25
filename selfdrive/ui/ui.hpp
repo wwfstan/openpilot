1abe557e85653ff9f621286a893ef33e251db332
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-07-25 18:06:09 +0900

    Update ui.hpp

--- selfdrive/ui/ui.hpp
+++ selfdrive/ui/ui.hpp
@@ -32,16 +32,23 @@
 #define NET_DISCONNECTED 1
 #define NET_ERROR 2
 
-#define COLOR_BLACK nvgRGBA(0, 0, 0, 255)
-#define COLOR_BLACK_ALPHA(x) nvgRGBA(0, 0, 0, x)
-#define COLOR_WHITE nvgRGBA(255, 255, 255, 255)
-#define COLOR_WHITE_ALPHA(x) nvgRGBA(255, 255, 255, x)
-#define COLOR_YELLOW nvgRGBA(218, 202, 37, 255)
-#define COLOR_RED nvgRGBA(201, 34, 49, 255)
-#define COLOR_OCHRE nvgRGBA(218, 111, 37, 255)
+# define COLOR_BLACK nvgRGBA(0, 0, 0, 255)
+# define COLOR_BLACK_ALPHA(x) nvgRGBA(0, 0, 0, x)
+# define COLOR_WHITE nvgRGBA(255, 255, 255, 255)
+# define COLOR_WHITE_ALPHA(x) nvgRGBA(255, 255, 255, x)
+# define COLOR_OCHRE nvgRGBA(218, 111, 37, 255)
+# define COLOR_OCHRE_ALPHA(x) nvgRGBA(218, 111, 37, x)
+# define COLOR_GREEN nvgRGBA(0, 255, 0, 255)
+# define COLOR_GREEN_ALPHA(x) nvgRGBA(0, 255, 0, x)
+# define COLOR_ORANGE nvgRGBA(255, 175, 3, 255)
+# define COLOR_ORANGE_ALPHA(x) nvgRGBA(255, 175, 3, x)
+# define COLOR_RED nvgRGBA(201, 34, 49, 255)
+# define COLOR_RED_ALPHA(x) nvgRGBA(201, 34, 49, x)
+# define COLOR_YELLOW nvgRGBA(218, 202, 37, 255)
+# define COLOR_YELLOW_ALPHA(x) nvgRGBA(218, 202, 37, x)
 
 #ifndef QCOM
-  #define UI_60FPS
+#define UI_60FPS
 #endif
 
 #define UI_BUF_COUNT 4
@@ -53,7 +60,8 @@ const int vwp_h = 1080;
 const int nav_w = 640;
 const int nav_ww= 760;
 const int sbr_w = 300;
-const int bdr_s = 30;
+const int bdr_s = 10;
+const int bdr_is = 30;
 const int box_x = sbr_w+bdr_s;
 const int box_y = bdr_s;
 const int box_w = vwp_w-sbr_w-(bdr_s*2);
@@ -72,7 +80,7 @@ const int home_btn_w = 180;
 const int home_btn_x = 60;
 const int home_btn_y = vwp_h - home_btn_h - 40;
 
-const int UI_FREQ = 30;   // Hz
+const int UI_FREQ = 30; // Hz
 
 const int MODEL_PATH_MAX_VERTICES_CNT = 98;
 const int MODEL_LANE_PATH_CNT = 3;
@@ -83,8 +91,8 @@ const int SET_SPEED_NA = 255;
 const uint8_t bg_colors[][4] = {
   [STATUS_STOPPED] = {0x07, 0x23, 0x39, 0xff},
   [STATUS_DISENGAGED] = {0x17, 0x33, 0x49, 0xff},
-  [STATUS_ENGAGED] = {0x17, 0x86, 0x44, 0xff},
-  [STATUS_WARNING] = {0xDA, 0x6F, 0x25, 0xff},
+  [STATUS_ENGAGED] = {0x17, 0x86, 0x44, 0x0f},
+  [STATUS_WARNING] = {0xDA, 0x6F, 0x25, 0x0f},
   [STATUS_ALERT] = {0xC9, 0x22, 0x31, 0xff},
 };
 
@@ -113,6 +121,12 @@ typedef struct UIScene {
   int ui_viz_rx;
   int ui_viz_rw;
   int ui_viz_ro;
+  
+  int lead_status;
+  float lead_d_rel, lead_y_rel, lead_v_rel;
+
+  int lead_status2;
+  float lead_d_rel2, lead_y_rel2, lead_v_rel2;
 
   int front_box_x, front_box_y, front_box_width, front_box_height;
 
@@ -120,7 +134,19 @@ typedef struct UIScene {
   std::string alert_text2;
   std::string alert_type;
   cereal::ControlsState::AlertSize alert_size;
-
+  // ui add
+  float angleSteers;
+  bool brakeLights;
+  float angleSteersDes;
+  bool steerOverride;
+  float output_scale; 
+  float cpu0Temp;
+  int batteryPercent;
+  bool batteryCharging;
+  char batteryStatus[64];
+  // ip addr
+  char ipAddr[20];
+  
   // Used to show gps planner status
   bool gps_planner_active;
 
@@ -168,6 +194,7 @@ typedef struct UIState {
   int img_turn;
   int img_face;
   int img_map;
+  int img_brake;
   int img_button_settings;
   int img_button_home;
   int img_battery;
@@ -235,6 +262,7 @@ typedef struct UIState {
   bool started;
   bool preview_started;
   bool vision_seen;
+  bool livempc_or_radarstate_changed;
 
   std::atomic<float> light_sensor;
 
