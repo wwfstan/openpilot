4fea62536e5b4bfe64cf3cec185b63efcc7645c7
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-07-31 19:31:54 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -107,7 +107,7 @@ class CarInterface(CarInterfaceBase):
 #      ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1525. + STD_CARGO_KG
       ret.wheelbase = 2.80
-      ret.steerRatio = 13.75
+      ret.steerRatio = 13.75 * 1.15
 #      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
 #      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
 
@@ -119,6 +119,8 @@ class CarInterface(CarInterfaceBase):
       #ret.lateralTuning.indi.timeConstant = 2.0
       #ret.lateralTuning.indi.actuatorEffectiveness = 1.7
 
+      tire_stiffness_factor = 0.385
+      
       ret.steerActuatorDelay = 0.08
       ret.steerLimitTimer = 0.4
       ret.steerRateCost = 0.25
@@ -221,13 +223,29 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
     elif candidate in [CAR.KIA_CARDENZA, CAR.KIA_CARDENZA_HEV]:
-      ret.lateralTuning.pid.kf = 0.00005
+      #ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1575. + STD_CARGO_KG
       ret.wheelbase = 2.85
-      ret.steerRatio = 12.5
-      ret.steerRateCost = 0.4
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
+      ret.steerRatio = 12.5      
+      #ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
+      #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.25], [0.05]]
+      
+      ret.steerActuatorDelay = 0.08
+      ret.steerLimitTimer = 0.4
+      ret.steerRateCost = 0.25
+      tire_stiffness_factor = 0.385
+      
+      ret.lateralTuning.init('lqr')
+
+      ret.lateralTuning.lqr.scale = 2000.0
+      ret.lateralTuning.lqr.ki = 0.01
+
+      ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+      ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+      ret.lateralTuning.lqr.c = [1., 0.]
+      ret.lateralTuning.lqr.k = [-100., 450.]
+      ret.lateralTuning.lqr.l = [0.22, 0.318]
+      ret.lateralTuning.lqr.dcGain = 0.003     
 
     ret.centerToFront = ret.wheelbase * 0.4
 
