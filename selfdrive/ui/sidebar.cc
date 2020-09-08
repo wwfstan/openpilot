860cded0af0c514dbaf0083989a9050334635790
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-09-08 18:48:43 +0900

    Update sidebar.cc

--- selfdrive/ui/sidebar.cc
+++ selfdrive/ui/sidebar.cc
@@ -19,19 +19,12 @@ static void ui_draw_sidebar_home_button(UIState *s) {
   ui_draw_image(s->vg, home_btn_x, home_btn_y, home_btn_w, home_btn_h, s->img_button_home, alpha);
 }
 
-static void ui_draw_sidebar_network_strength(UIState *s) {
-  static std::map<cereal::ThermalData::NetworkStrength, int> network_strength_map = {
-      {cereal::ThermalData::NetworkStrength::UNKNOWN, 1},
-      {cereal::ThermalData::NetworkStrength::POOR, 2},
-      {cereal::ThermalData::NetworkStrength::MODERATE, 3},
-      {cereal::ThermalData::NetworkStrength::GOOD, 4},
-      {cereal::ThermalData::NetworkStrength::GREAT, 5}};
-  const int network_img_h = 27;
-  const int network_img_w = 176;
-  const int network_img_x = 58;
-  const int network_img_y = 196;
-  const int img_idx = s->scene.thermal.getNetworkType() == cereal::ThermalData::NetworkType::NONE ? 0 : network_strength_map[s->scene.thermal.getNetworkStrength()];
-  ui_draw_image(s->vg, network_img_x, network_img_y, network_img_w, network_img_h, s->img_network[img_idx], 1.0f);
+static void ui_draw_sidebar_ipaddress(UIState *s) {
+  const int ipaddress_x = 58;
+  const int ipaddress_y = 196;
+  nvgFillColor(s->vg, COLOR_LIME);
+  nvgFontSize(s->vg, 30);
+  nvgTextBox(s->vg, ipaddress_x-20, ipaddress_x + 55, 250, s->scene.thermal.getWifiIpAddress().cStr(), NULL);
 }
 
 static void ui_draw_sidebar_battery_icon(UIState *s) {
@@ -65,10 +58,6 @@ static void ui_draw_sidebar_network_type(UIState *s) {
   nvgFontFaceId(s->vg, s->font_sans_regular);
   nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
   nvgTextBox(s->vg, network_x, network_y, network_w, network_type ? network_type : "--", NULL);
-  
-  nvgFillColor(s->vg, COLOR_LIME);
-  nvgFontSize(s->vg, 30);
-  nvgTextBox(s->vg, network_x-20, network_y + 55, 250, s->scene.thermal.getWifiIpAddress().cStr(), NULL);
 }
 
 static void ui_draw_sidebar_metric(UIState *s, const char* label_str, const char* value_str, const int severity, const int y_offset, const char* message_str) {
@@ -116,37 +105,6 @@ static void ui_draw_sidebar_metric(UIState *s, const char* label_str, const char
   }
 }
 
-static void ui_draw_sidebar_metric2(UIState *s, const int severity, const int y_offset, const char* message_str) {
-  const int metric_x = !s->scene.uilayout_sidebarcollapsed ? 30 : -(sbr_w);
-  const int metric_y = 338 + y_offset;
-  const int metric_w = 240;
-  const int metric_h = message_str ? strchr(message_str, '\n') ? 124 : 100 : 148;
-
-  NVGcolor status_color;
-
-  if (severity == 0) {
-    status_color = COLOR_WHITE;
-  } else if (severity == 1) {
-    status_color = COLOR_YELLOW;
-  } else if (severity > 1) {
-    status_color = COLOR_RED;
-  }
-
-  ui_draw_rect(s->vg, metric_x, metric_y, metric_w, metric_h,
-               severity > 0 ? COLOR_WHITE : COLOR_WHITE_ALPHA(85), 20, 2);
-
-  nvgBeginPath(s->vg);
-  nvgRoundedRectVarying(s->vg, metric_x + 6, metric_y + 6, 18, metric_h - 12, 25, 0, 0, 25);
-  nvgFillColor(s->vg, status_color);
-  nvgFill(s->vg);
-
-  nvgFillColor(s->vg, COLOR_WHITE);
-    nvgFontSize(s->vg, 78*0.8);
-    nvgFontFaceId(s->vg, s->font_sans_bold);
-    nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
-    nvgTextBox(s->vg, metric_x + 35, metric_y + (strchr(message_str, '\n') ? 40 : 50), metric_w - 50, message_str, NULL);
-  }
-
 static void ui_draw_sidebar_temp_metric(UIState *s) {
   static std::map<cereal::ThermalData::ThermalStatus, const int> temp_severity_map = {
       {cereal::ThermalData::ThermalStatus::GREEN, 0},
@@ -156,13 +114,13 @@ static void ui_draw_sidebar_temp_metric(UIState *s) {
   char temp_label_str[32];
   char temp_value_str[32];
   char temp_value_unit[32];
-  const int temp_y_offset = 45;
+  const int temp_y_offset = 0;
   snprintf(temp_value_str, sizeof(temp_value_str), "%d", s->scene.thermal.getPa0());
   snprintf(temp_value_unit, sizeof(temp_value_unit), "%s", "°C");
-  temp_label_str[0] = 0;
+  snprintf(temp_label_str, sizeof(temp_label_str), "%s", "시스템 온도");
   strcat(temp_value_str, temp_value_unit);
 
-  ui_draw_sidebar_metric2(s, temp_severity_map[s->scene.thermal.getThermalStatus()], temp_y_offset, temp_value_str);
+  ui_draw_sidebar_metric(s, temp_label_str, temp_value_str, temp_severity_map[s->scene.thermal.getThermalStatus()], temp_y_offset, NULL);
 }
 
 static void ui_draw_sidebar_panda_metric(UIState *s) {
@@ -208,7 +166,7 @@ void ui_draw_sidebar(UIState *s) {
   }
   ui_draw_sidebar_settings_button(s);
   ui_draw_sidebar_home_button(s);
-  ui_draw_sidebar_network_strength(s);
+  ui_draw_sidebar_ipaddress(s);
   ui_draw_sidebar_battery_icon(s);
   ui_draw_sidebar_network_type(s);
   ui_draw_sidebar_temp_metric(s);
