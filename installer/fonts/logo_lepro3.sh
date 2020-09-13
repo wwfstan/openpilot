c3fe9d932cde03172273798ecafdb89e4039808a
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-09-13 13:25:04 +0900

    Update and rename logo_lepro3 to logo_lepro3.sh

--- installer/fonts/logo_lepro3.sh
+++ installer/fonts/logo_lepro3.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
 echo =================================================================
-echo comma boot logo & bootanimation change by lepro3
+echo comma boot logo and bootanimation change by lepro3
 echo =================================================================
 dd if=./splash.img of=/dev/block/bootdevice/by-name/splash
 mount -o rw,remount /system
 cp ./bootanimation.zip /system/media/
+
+echo =================================================================
+echo complete !
+echo =================================================================
