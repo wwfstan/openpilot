730e2b0620aa156955b668afe91700467ed16f6b
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-09-13 13:23:53 +0900

    Update and rename logo_op3t to logo_op3t.sh

--- installer/fonts/logo_op3t.sh
+++ installer/fonts/logo_op3t.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
 echo =================================================================
-echo comma boot logo & bootanimation change by op3t
+echo comma boot logo and bootanimation change by op3t
 echo =================================================================
+
 dd if=./logo.bin of=/dev/block/bootdevice/by-name/LOGO
 mount -o rw,remount /system
 cp ./bootanimation.zip /system/media/
+
+echo complete
