daec98c012005dfa20cb04701ad93e27a71468ca
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-09-13 13:21:51 +0900

    Delete op3t_logo.sh

--- installer/fonts/op3t_logo.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-echo =================================================================
-echo comma boot logo & bootanimation change by op3t
-echo =================================================================
-dd if=./logo.bin of=/dev/block/bootdevice/by-name/LOGO
-mount -o rw,remount /system
-cp ./bootanimation.zip /system/media/
