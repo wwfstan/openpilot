96e0e557a07801c23b640059911ecc23deff83d5
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-09-13 13:20:36 +0900

    Update op3t_logo.sh

--- installer/fonts/op3t_logo.sh
+++ installer/fonts/op3t_logo.sh
@@ -6,9 +6,3 @@ echo =================================================================
 dd if=./logo.bin of=/dev/block/bootdevice/by-name/LOGO
 mount -o rw,remount /system
 cp ./bootanimation.zip /system/media/
-echo =================================================================
-echo = eon by lepro3
-echo dd if=./splash.img of=/dev/block/bootdevice/by-name/splash
-echo mount -o rw,remount /system
-echo cp ./bootanimation.zip /system/media/
-echo =================================================================
