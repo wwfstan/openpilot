fd207a99f1a3b99d93b674ee230756870801cef2
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-10-04 00:26:57 +0900

    shutdown add

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -189,6 +189,7 @@ managed_processes = {
   "dmonitoringmodeld": ("selfdrive/modeld", ["./dmonitoringmodeld"]),
   "modeld": ("selfdrive/modeld", ["./modeld"]),
   "rtshield": "selfdrive.rtshield",
+  "shutdownd": "selfdrive.shutdownd",  
 }
 
 daemon_processes = {
@@ -228,6 +229,7 @@ if not PC:
 if ANDROID:
   persistent_processes += [
     'updated',
+    'shutdownd',    
   ]
 
 car_started_processes = [
@@ -448,6 +450,9 @@ def manager_init(should_register=True):
     os.chmod(os.path.join(BASEDIR, "cereal", "libmessaging_shared.so"), 0o755)
 
 def manager_thread():
+  shutdownd = Process(name="shutdownd", target=launcher, args=("selfdrive.shutdownd",))
+  shutdownd.start()
+  
   # now loop
   thermal_sock = messaging.sub_sock('thermal')
 
