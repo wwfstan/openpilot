07cbd75eaaad7abc6c80d5e2d4ebd18b99d08cc6
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-10-03 18:05:25 +0900

    toggle 4

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -185,7 +185,12 @@ class CarInterface(CarInterfaceBase):
     ret.evgearAvailable = True if 882 in fingerprint[0] else False
     ret.emsAvailable = True if 608 and 809 in fingerprint[0] else False
   
-    ret.sccBus = 0 if 1057 in fingerprint[0] else 2 if 1057 in fingerprint[2] else -1
+    #ret.sccBus = 0 if 1057 in fingerprint[0] else 2 if 1057 in fingerprint[2] else -1
+    if Params().get('SccEnabled') == b'1':
+      ret.sccBus = 2 if 1057 in fingerprint[2] else 0
+    else:
+      ret.sccBus = -1
+      
     ret.radarOffCan = (ret.sccBus == -1)
     ret.radarTimeStep = 0.02
 
