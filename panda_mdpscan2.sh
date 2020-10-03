d6ae1942f559f7e486edb8820bc63ce8f2f7ca5c
Lee Jong Mun <43285072+crwusiz@users.noreply.github.com>
2020-10-03 15:11:22 +0900

    Update panda_mdpscan2.sh

--- panda_mdpscan2.sh
+++ panda_mdpscan2.sh
@@ -5,4 +5,4 @@ export HOME=/data/data/com.termux/files/home
 export PATH=/usr/local/bin:/data/data/com.termux/files/usr/bin:/data/data/com.termux/files/usr/sbin:/data/data/com.termux/files/usr/bin/applets:/bin:/sbin:/vendor/bin:/system/sbin:/system/bin:/system/xbin:/data/data/com.termux/files/usr/bin/python
 export PYTHONPATH=/data/openpilot
 
-cd /data/openpilot/panda/board/mdpscan1 cp safety_declarations.h /data/openpilot/panda/board && reboot
+cd /data/openpilot/panda/board/mdpscan1 cp safety_declarations.h /data/openpilot/panda/board; reboot
